e50279a7b8668b7081b90411c6d2c63b5e3172eb
Tabrez2 <72229863+Tabrez2@users.noreply.github.com>
2024-09-08 13:20:25 +0530

    Using  Lambda expression

--- src/main/java/tutorial/Main.java
+++ src/main/java/tutorial/Main.java
@@ -11,37 +11,50 @@ public class Main {
 		//System.out.println(hotelService.filterHotelByPrice(4000)); 
 //		System.out.println(hotelService.filterHotelByStarRating(ht)); 
 	
-		 HotelService hotelService = new HotelService();
-		 int price = 6000;  // Ensure price is not modified
-
-		 List<Hotel> hotelsByPrice = hotelService.filterHotels(new FilteringCondition() {
-		     @Override
-		     public boolean test(Hotel hotel) {
-		         return hotel.getPricePerNight() <= price;
-		     }
-		 });
+//		 HotelService hotelService = new HotelService();
+//		 int price = 6000;  // Ensure price is not modified
+//
+//		 List<Hotel> hotelsByPrice = hotelService.filterHotels(new FilteringCondition() {
+//		     @Override
+//		     public boolean test(Hotel hotel) {
+//		         return hotel.getPricePerNight() <= price;
+//		     }
+//		 });
+		 
+    	 HotelService hotelService = new HotelService();
+		 int price=2000;
+     	 List<Hotel> hotelsByPrice = hotelService.filterHotels((Hotel hotel)->{//here using lambda exp
+     		 return hotel.getPricePerNight()<=price;
+     	 });
 		 System.out.println(hotelsByPrice);
 		 
 System.out.println("--------------------------------------------------------------------------------------------------");
 	 
-	 HotelType ht = HotelType.FIVE_STAR;
+//	 HotelType ht = HotelType.FIVE_STAR;
+//
+//	 List<Hotel>hotels=hotelService.filterHotels(new  FilteringCondition() {
+//		
+//
+//			@Override
+//			public boolean test(Hotel hotel) {
+//				return hotel.getHotelType()==ht;
+//			}
+//			
+//	 });
+//	 System.out.println(hotels);
+HotelType ht = HotelType.THREE_STAR;
 
-	 List<Hotel>hotels=hotelService.filterHotels(new  FilteringCondition() {
-		
+List<Hotel>hotels=hotelService.filterHotels((Hotel hotel)-> {//here using lambda exp
+		return hotel.getHotelType()==ht;		
+});
+System.out.println(hotels);
 
-			@Override
-			public boolean test(Hotel hotel) {
-				return hotel.getHotelType()==ht;
-			}
-			
-	 });
-	 System.out.println(hotels);
 	 
 /*Passing function in java
  * 
  * 1.create an interface
  * 2.instantiate a class  that implements the interface
- * 3.implement the method of the interfas=ce ,
+ * 3.implement the method of the interface ,
  *   where the method body = function body which 
  *   you wanted to pass as parameter
  * 4.pass an object of the concrete implementation of the interface  
